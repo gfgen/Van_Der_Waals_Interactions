4f5540b0f3e5dcf5993e2a7b5631eb245c9bfabe
Nguyen Phuc Nguyen <phucnguyen1999.nn@gmail.com>
2021-06-06 12:30:04 -0400

    Added Fly cam

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate nalgebra as na;
 extern crate ndarray;
 extern crate itertools;
 extern crate bevy;
+extern crate bevy_flycam;
 extern crate rand;
 
 mod state; 
@@ -13,6 +14,7 @@ mod physics;
 mod interactivity;
 
 use bevy::prelude::*;
+use bevy_flycam::PlayerPlugin;
 use state::particle::Particle;
 
 // TODO: Clean up main.rs
@@ -20,6 +22,7 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    state: Res<state::State>
 ) {
     let sphere_mesh = meshes.add(Mesh::from(shape::Icosphere { 
                 radius: 0.1,
@@ -27,7 +30,8 @@ fn setup(
     }));
     let white_mat = materials.add(Color::rgb(1., 0.9, 0.9).into());
     // sphere
-    for _i in 0..30 {
+    let n = state.particles.len();
+    for _i in 0..n {
         commands.spawn()
             .insert_bundle(PbrBundle {
                 mesh: sphere_mesh.clone(),
@@ -38,18 +42,17 @@ fn setup(
     }
 
     // Camera
-    commands.spawn()
+/*     commands.spawn()
         .insert_bundle(PerspectiveCameraBundle {
-            transform: Transform::from_matrix(Mat4::from_rotation_translation(
-                Quat::from_xyzw(-0.3, -0.5, -0.3, 0.5).normalize(),
-                Vec3::new(-7.0, 20.0, 4.0))),
+            transform: Transform::from_matrix(Mat4::from_translation(
+                Vec3::new(0.0, 20.0, 4.0))),
             ..Default::default()
-        });
+        }); */
 
         // Light
     commands.spawn()
         .insert_bundle(LightBundle {
-            transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
+            transform: Transform::from_translation(Vec3::new(4.0, 8.0, 10.0)),
             ..Default::default()
         });
 }
@@ -68,13 +71,32 @@ fn update_state(
     }
 }
 
-fn main() -> Result<(), state::error::InvalidParamError> {
-    let mut particles = vec![];
-    for _i in 0..30 {
-        particles.push(Particle::new()
-            .set_pos(rand::random::<f64>() * 1.5 + 2.0, rand::random::<f64>() * 1.5 + 2.0, rand::random::<f64>() * 1.5 + 2.0));
+// TODO: Move somewhere more appropriate
+fn prune(particles: Vec<Particle>) -> Vec<Particle> {
+    let mut ret: Vec<Particle> = vec![];
+    for p1 in particles.iter() {
+        let mut qual = true;
+        for p2 in ret.iter() {
+            let r = p1.get_pos() - p2.get_pos();
+            let rnorm = r.norm();
+            if rnorm == 0.0 { continue; }
+            qual = qual && rnorm >= 0.15
+        } 
+        if qual { ret.push(p1.clone()); }
     }
-    let state = state::StatePrototype::new().set_particles(particles).compile()?;
+    ret
+}
+
+fn main() -> Result<(), state::error::InvalidParamError> {
+    let temp = 1.0;
+    let mut particles = vec![];
+    for _i in 0..1000 {
+        particles.push(Particle::new()
+            .set_pos(rand::random::<f64>() * 1.5 + 2.0, rand::random::<f64>() * 1.5 + 2.0, rand::random::<f64>() * 1.5 + 2.0))
+            .set_vel(rand::random);
+    }
+    let particles = prune(particles);
+    let state = state::StatePrototype::new().set_bound_x(10.0).set_bound_y(10.0).set_particles(particles).compile()?;
 
     App::build()
         .add_startup_system(setup.system())
@@ -89,7 +111,7 @@ fn main() -> Result<(), state::error::InvalidParamError> {
         })
         .insert_resource(state)
         .add_system(update_state.system())
-        .add_plugin(interactivity::camera_panning::CameraPanning)
+        .add_plugin(PlayerPlugin)
         .add_plugins(DefaultPlugins)
         .run();
 
